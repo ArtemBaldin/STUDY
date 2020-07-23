dde43a8c961cd5faa1ca582a13cd1d92478a0ad8
ArtemBaldin <artem.s.b7@gmail.com>
2020-07-24 00:06:01 +0300

    Homework_9_f_flake8

--- PYTHON_INTRODUCTION/HOMEWORKS/Home_work_9/Home_work_9_f.py
+++ PYTHON_INTRODUCTION/HOMEWORKS/Home_work_9/Home_work_9_f.py
@@ -1,44 +1,52 @@
+import datetime
 """
 1
 Input: Feb 12 2019 2:41PM
 Output: 2019-02-12 14:41:00
 Функция принимает строку (пример - Input) и возвращает строку (пример - Output)
 """
-print ("Task_1\n")
-import datetime
+print("Task_1\n")
 
-def date_transform_format (date_string):
+
+def date_transform_format(date_string):
     return datetime.datetime.strptime(date_string, "%b %d %Y %I:%M%p")
 
-print (date_transform_format("Feb 12 2019 2:41PM"))
+
+print(date_transform_format("Feb 12 2019 2:41PM"))
 
 """
 2
-Напишите функция is_prime, которая принимает 1 аргумент (число) и возвращает True, если число простое, иначе False
-Простое число - это число, которое делится без остатка только на себя и на 1
+Напишите функция is_prime, которая принимает \
+1 аргумент (число) и возвращает True, если число \
+простое, иначе False
+Простое число - это число, которое делится без \
+остатка только на себя и на 1
 """
 print("\nTask_2\n")
-def is_prime (num):
-    increment_num=3
-    if num<2:
+
+
+def is_prime(num):
+    increment_num = 3
+    if num < 2:
         return False
     while increment_num < num:
-            if num%increment_num==0 or num%2==0:
-                return False
-            increment_num +=2
+        if num % increment_num == 0 or num % 2 == 0:
+            return False
+            increment_num += 2
     return True
-    
-   
-digit_to_check=1000000
-for i in range (100):
-    if is_prime(digit_to_check)==True:
-        print (f"{i} - {digit_to_check} - {is_prime(digit_to_check)}")
-    digit_to_check +=1
+
+
+digit_to_check = 1000000
+for i in range(100):
+    if is_prime(digit_to_check) is True:
+        print(f"{i} - {digit_to_check} - {is_prime(digit_to_check)}")
+    digit_to_check += 1
 
 
 """
 3
-Напишите функцию, которая принимает 1 аргумент (строка) и выполняет следующие действия на каждую из букв строки:
+Напишите функцию, которая принимает 1 аргумент (строка)\
+ и выполняет следующие действия на каждую из букв строки:
 i - инкремент (+1)
 d - дикремент (-1)
 s - возведение в квадрат
@@ -51,21 +59,22 @@ parse("iiisdoso")  ==>  [8, 64]
 """
 print("\nTask_3\n")
 
+
 def parse_str(str_symb):
-    num=0
-    res=[]
+    num = 0
+    res = []
     for symb in str_symb:
-        if symb=="i":
+        if symb == "i":
             num += 1
-        elif symb=="d":
-            num -=1
-        elif symb=="s":
-            num=num**2
+        elif symb == "d":
+            num -= 1
+        elif symb == "s":
+            num = num**2
         elif symb == "o":
             res.append(num)
-        
     return res
-    
-to_do="iiisdoso"
+
+
+to_do = "iiisdoso"
 
 print(parse_str(to_do))
